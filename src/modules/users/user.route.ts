463a252626a86a51eee2b365a7fbe3a2d64d78a9
Md. Jubayer Shikder <validjubayer@gmail.com>
2025-12-05 16:51:21 +0600

    feat: add method DELETE /users/:userId route

--- src/modules/users/user.route.ts
+++ src/modules/users/user.route.ts
@@ -1,9 +1,41 @@
 import { Router } from "express";
 import { userControllers } from "./user.controller";
+import { pool } from "../../config/db";
+import { Request, Response } from "express";
 
 const router = Router();
 
 // admin_access_only middleware needed
 router.get("/", userControllers.findAll);
+
+// admin_access_only middleware needed
+router.delete("/:userId", async (req: Request, res: Response) => {
+  const uid = req.params.userId;
+  const result = await pool.query(
+    `
+        DELETE FROM users WHERE id=$1
+        `,
+    [uid],
+  );
+
+  try {
+    if (!result.rowCount) {
+      res.status(404).json({
+        success: false,
+        message: "user not found",
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        message: "User deleted successfully",
+      });
+    }
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
 
 export const userRoutes = router;
