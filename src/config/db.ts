347129add49a23c855babc195082069956f3205e
Md. Jubayer Shikder <validjubayer@gmail.com>
2025-12-06 18:31:03 +0600

    fix(db): remove  DEFAULT NOW() from bookings table rent_start_date column

--- src/config/db.ts
+++ src/config/db.ts
@@ -66,7 +66,7 @@ DO $$
           id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
           customer_id INT REFERENCES users(id) ON DELETE CASCADE,
           vehicle_id INT REFERENCES vehicles(id) ON DELETE CASCADE,
-          rent_start_date DATE NOT NULL DEFAULT NOW() ,
+          rent_start_date DATE NOT NULL,
           rent_end_date DATE NOT NULL,
              CHECK (rent_end_date > rent_start_date),
           total_price INT NOT NULL,
