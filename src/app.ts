17e54aae7fefbe0db17e67b21c571b689968dd48
Md. Jubayer Shikder <validjubayer@gmail.com>
2025-12-07 23:11:40 +0600

    feat: add route not found error

--- src/app.ts
+++ src/app.ts
@@ -4,20 +4,18 @@ import { userRoutes } from "./modules/users/user.route";
 import { vehicleRoutes } from "./modules/vehicles/vehicle.route";
 import { bookingRoutes } from "./modules/bookings/booking.route";
 import { authRoutes } from "./modules/auth/auth.route";
+import { notFound } from "./utils/responseHandler";
 
 const app: Application = express();
 app.use(express.json());
 
-
 const apiV1 = Router();
 app.use("/api/v1/", apiV1);
 
-
 // "/api/v1/" routes
 apiV1.get("/", (_req: Request, res: Response) => {
-  res.send("Hello from vehicle rental system nested api v1 route ");
+  res.send("Don't worry, the app is running like butter smooth.");
 });
-
 apiV1.use("/users", userRoutes);
 apiV1.use("/vehicles", vehicleRoutes);
 apiV1.use("/bookings", bookingRoutes);
@@ -29,5 +27,13 @@ initDB()
   .catch((err) => {
     console.error(err);
   });
+
+//route not found
+app.use((req: Request, res: Response) => {
+  return notFound(
+    res,
+    `route '${req.path}' not found. Navigate from path '/api/v1/your_intended_route'`,
+  );
+});
 
 export default app;
